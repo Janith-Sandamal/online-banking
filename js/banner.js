206932dd153fcb28e4205379b3728b0b384589de
Janith Sandamal <78975250+Janith-Sandamal@users.noreply.github.com>
2022-05-16 18:21:22 +0530

    Banner error fix hosting

--- js/banner.js
+++ js/banner.js
@@ -1,6 +1,6 @@
 function slider(){
     var i=0;
-    var images=["./images/banner1.png","./images/banner2.png","./images/banner3.png","./images/banner4.png","./images/banner5.png"];
+    var images=["../images/banner1.png","../images/banner2.png","../images/banner3.png","../images/banner4.png","../images/banner5.png"];
     var time=3000;
     function changeImg(){
         document.getElementById("slideimg").src=images[i];
