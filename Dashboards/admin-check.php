ffe2930deec512c507746c1f090d3fc86b3ab582
PrabodaSankalpa <dgprabodasankalpa@gmail.com>
2022-05-22 22:58:03 +0530

    User request accpet done✅

--- Dashboards/admin-check.php
+++ Dashboards/admin-check.php
@@ -2,13 +2,47 @@
 require '../lib/db.php';
 
 
-//admin user details check if form is submitted
-$required = array('username', 'password');
+if (isset($_POST['submit'])) {
+    $errors = array();
 
-?> 
+    if ($_POST['searchBy'] == 'nic') {
+        if (!isset($_POST['nic']) || strlen(trim($_POST['nic'])) < 9) {
+            $errors[] = "NIC is Invalid or Missing!";
+        }
+
+        if (empty($errors)) {
+            $nic = $_POST['nic'];
+            $query = "SELECT * FROM customers WHERE nic='{$nic}' LIMIT 1;";
+            $result = mysqli_query($connection, $query);
+            if (mysqli_num_rows($result) == 1) {
+                $data = mysqli_fetch_assoc($result);
+            } else {
+                $errors[] = "No Customer with a given NIC";
+            }
+        }
+    } elseif ($_POST['searchBy'] == 'email') {
+        if (!isset($_POST['email']) || strlen(trim($_POST['email'])) < 1) {
+            $errors[] = "E-mail is Invalid or Missing!";
+        }
+
+        if (empty($errors)) {
+            $email = $_POST['email'];
+            $query = "SELECT * FROM customers WHERE email='{$email}' LIMIT 1;";
+            $result = mysqli_query($connection, $query);
+            if (mysqli_num_rows($result) == 1) {
+                $data = mysqli_fetch_assoc($result);
+            } else {
+                $errors[] = "No Customer with a given E-mail";
+            }
+        }
+    }
+}
+
+?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -21,22 +55,23 @@ $required = array('username', 'password');
 
     <!-- Link Normalize CSS file -->
     <link rel="stylesheet" href="../css/Normalize.css">
-    
+
     <!-- Google Fonts -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap">
-    
+
 
     <title>User Dashboard</title>
 </head>
+
 <body>
     <div class="side-menu">
         <div class="brand-name">
             <!-- <h1>Brand</h1> -->
         </div>
         <ul>
-            <li><img src="https://i.ibb.co/q5Xj3NW/dashboard-1.png" alt="">&nbsp; <span><a href="./admin.php" >Dashboard</a></span> </li>
+            <li><img src="https://i.ibb.co/q5Xj3NW/dashboard-1.png" alt="">&nbsp; <span><a href="./admin.php">Dashboard</a></span> </li>
             <li><img src="https://i.ibb.co/frgxY9k/refresh.png" alt="">&nbsp;<span><a href="./admin-update.php">Update</a></span> </li>
-            <li><img src="https://i.ibb.co/x7fg256/checked.png" alt="">&nbsp;<span><a href="./admin-check.php" >Check</a></span> </li>
+            <li><img src="https://i.ibb.co/x7fg256/checked.png" alt="">&nbsp;<span><a href="./admin-check.php">Check</a></span> </li>
             <li><img src="https://i.ibb.co/jhNDBNb/settings-2.png" alt="">&nbsp;<span><a href="./admin-settings.php">Settings</a></span> </li>
             <li><img src="https://i.ibb.co/vsK6jgH/information.png" alt="">&nbsp;<span><a href="#">Help</a></span> </li>
             <li><img src="https://i.ibb.co/8gznnxt/logout-1.png" alt="">&nbsp; <span><a href="./logout.php">Log Out</a></span></li>
@@ -45,12 +80,12 @@ $required = array('username', 'password');
     <div class="container">
         <div class="header">
             <div class="nav">
-                
+
                 <div class="user">
-                    
-                    
+
+
                     <div class="img-case">
-                        
+
                     </div>
                 </div>
             </div>
@@ -59,45 +94,67 @@ $required = array('username', 'password');
 
             <div class="cards">
             </div>
-            
+
             <div class="content-2">
 
                 <!-- Search User -->
                 <div class="details-update">
                     <div class="title">
-                        <h2>Users Details Search</h2>   
+                        <h2>Users Details Search</h2>
                     </div>
+                    <?php
+
+                    if (!empty($errors)) {
+                        echo '<p style="color:red; margin:10px 0 0 10px;">';
+                        foreach ($errors as $error) {
+                            echo $error . '<br>';
+                        }
+                        echo '</p>';
+                    }
+
+
+                    ?>
                     <table>
-                        <form action="">
+                        <form action="./admin-check.php" method="POST">
 
                             <tr>
                                 <td>
                                     <label for="nic">NIC*</label><br>
-                                    <input type="text" name="nic" id="nic" placeholder="Enter NIC Number" >
+                                    <input type="text" name="nic" id="nic" placeholder="Enter NIC Number">
                                 </td>
 
                                 <td>
                                     <label for="email">Email*</label><br>
-                                    <input type="email" name="email" id="email" placeholder="Enter Email Address" >
+                                    <input type="email" name="email" id="email" placeholder="Enter Email Address">
                                 </td>
                             </tr>
 
                             <tr>
                                 <td>
-                                    <label for="username">Username*</label><br>
-                                    <input type="text" name="username" id="username" placeholder="Enter Username" >
+                                    <label>Search by*</label><br>
+                                    <input type="radio" name="searchBy" value="nic" checked> NIC &nbsp;
+                                    <input type="radio" name="searchBy" value="email"> E-mail
                                 </td>
 
-                                
+
                             </tr>
 
-                            
+
 
                             <tr>
                                 <td>
-                                    <input type="submit" name="submit" value="Confirm &rarr;">
+                                    <input type="submit" name="submit" value="Search &rarr;">
                                 </td>
                             </tr>
+
+                            <?php
+
+                            if (empty($errors)) {
+                                echo "<tr>"
+                                    . "<td>" . $data['title'] . " " . $data['frist_name'] . "</td>";
+                            }
+
+                            ?>
 
 
                         </form>
@@ -111,4 +168,5 @@ $required = array('username', 'password');
         </div>
     </div>
 </body>
+
 </html>
